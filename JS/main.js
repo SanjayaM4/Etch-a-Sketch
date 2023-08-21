cc3cbe1ec2bd36b2beaf575beaadd1d58f90e828
SanjayaM4 <sanjaya5maraj@gmail.com>
2023-08-21 04:44:36 -0400

    setup square grid with mouseover functionality

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 function newGrid(size)
 {
     size = size || 16;
-    cellSize = 40/size + "rem";
+    cellSize = 40/Math.sqrt(size) + "rem";
 
     const mainContainerDiv = document.getElementById("maincontainer");
 
@@ -11,7 +11,16 @@ function newGrid(size)
     {
         divArray[i] = document.createElement("div");
 
+        divArray[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+
+        divArray[i].style.width = cellSize;
+        divArray[i].style.height = cellSize;
+
         mainContainerDiv.appendChild(divArray[i]);
     }
 
 }
+
+newGrid(16);
